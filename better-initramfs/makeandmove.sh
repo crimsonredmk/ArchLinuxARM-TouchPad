f92bee1991243ceff2e94fa390028f1458ed6759
Michael Staszel <crimsonredmk@gmail.com>
2011-12-16 21:12:52 -0600

    Getting back into it

--- better-initramfs/makeandmove.sh
+++ better-initramfs/makeandmove.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script makes the initramfs image and moves it to my kernel directory.
 rm ../hp-kernel-tenderloin/img.gz
 make image
 mv initramfs.cpio.gz ../hp-kernel-tenderloin/img.gz

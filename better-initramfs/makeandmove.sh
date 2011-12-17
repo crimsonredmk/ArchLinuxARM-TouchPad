19b1556ded0de95b2f76e84b593ab05eaa64448d
Michael Staszel <crimsonredmk@gmail.com>
2011-12-17 11:34:16 -0600

    changed lvm name to root-arch

--- better-initramfs/makeandmove.sh
+++ better-initramfs/makeandmove.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # This script makes the initramfs image and moves it to my kernel directory.
-rm ../hp-kernel-tenderloin/img.gz
+rm ../../hp-kernel-tenderloin/img.gz
 make image
-mv initramfs.cpio.gz ../hp-kernel-tenderloin/img.gz
+mv initramfs.cpio.gz ../../hp-kernel-tenderloin/img.gz
